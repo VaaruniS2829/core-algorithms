c8534ebb3aa85b9a1660c6bd8e8c76f4e56a7772
Vaaruni S <vaarunis20@gmail.com>
2025-11-14 22:55:27 +0530

    PrimeCheck.java

---
+++ PrimeCheck.java
@@ -0,0 +1,20 @@
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int n = 17;
+        boolean isPrime = true;
+
+        if (n <= 1) isPrime = false;
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not Prime");
+    }
+}
