82fd6b160b4969b937973fb8ef4969f655aa0a69
Vaaruni S <vaarunis20@gmail.com>
2025-11-16 23:32:53 +0530

    CountVowels.java

---
+++ CountVowels.java
@@ -0,0 +1,13 @@
+class CountVowels {
+    public static void main(String[] args) {
+        String s = "education";
+        int count = 0;
+
+        for (char ch : s.toCharArray()) {
+            if ("aeiouAEIOU".indexOf(ch) != -1)
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
